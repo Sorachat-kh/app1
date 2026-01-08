42eb9d4e753009acd64740d58374b37f70a0b950
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-01-08 11:44:51 +0700

    event data validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Calculator2 } from './calculator';
 import Button from './class-components';
 import Calendar from './class-components';
+import { EventData2 } from './event-data';
 import { Content, Footer, Header } from './func-components';
 import logo from './logo.svg';
 
 
 function App() {
-  return <Calculator2/>
+  return <EventData2/>
 }
 
 export default App;
