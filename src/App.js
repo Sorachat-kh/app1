f1d6ad69ee772abf888760cbb29e5a9bdce053ac
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-01-15 10:18:29 +0700

    arraydata

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Calendar from './class-components';
 import { EventData2 } from './event-data';
 import { Content, Footer, Header } from './func-components';
 import logo from './logo.svg';
+import RefsArray from './refs-array';
+import RefsFunc from './refs-func';
+import React from 'react';
 
 
 function App() {
-  return <EventData2/>
+  return <RefsArray/>
 }
 
 export default App;
