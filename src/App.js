e1bc235527c32840d822a4fed563ca53e19b6797
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-01-29 11:07:21 +0700

    context-state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { userContext } from './context'
-import Content from './context-content'
+import Content from './context-content2'
+import Header2 from './context-header2'
 export default function App() {
+  let [user, setUser] = React.useState('')
   return (
-    <userContext.Provider value={'Tom Jerry'}>
+    <userContext.Provider value={[user, setUser]}>
+    <Header2/>
     <Content/>
     </userContext.Provider>
   )
