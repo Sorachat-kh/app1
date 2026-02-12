b6dbebef1c49d5c1c30716616d2e01d9735b2338
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-02-12 10:02:27 +0700

    router2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React from 'react'
 import { userContext } from './context'
 import Content from './context-content2'
 import Header2 from './context-header2'
+
+import Router2 from './router2'
+
 export default function App() {
-  let [user, setUser] = React.useState('')
-  return (
-    <userContext.Provider value={[user, setUser]}>
-    <Header2/>
-    <Content/>
-    </userContext.Provider>
-  )
+  return<Router2/>
 }
