c4bc6cfd7bc86cdae48382611f016204dd311686
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-02-20 19:56:25 +0700

    formtext

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { userContext } from './context'
 import Content from './context-content2'
 import Header2 from './context-header2'
 
+
 import Router2 from './router2'
 import Router3 from './router3'
+import FormText from './form-text'
 
 export default function App() {
-  return<Router3/>
+  return<FormText/>
 }
