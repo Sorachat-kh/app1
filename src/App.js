f4f86715229a75a8cc9d569783e0b5f1bc05a1bc
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-02-20 18:25:54 +0700

    routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Content from './context-content2'
 import Header2 from './context-header2'
 
 import Router2 from './router2'
+import Router3 from './router3'
 
 export default function App() {
-  return<Router2/>
+  return<Router3/>
 }
