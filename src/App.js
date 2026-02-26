c93ff7673cb5e652a3c2e02f125261f141b611a8
Sorachat-kh <s6703052411261@email.kmutnb.ac.th>
2026-02-26 10:28:25 +0700

    selectfile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Header2 from './context-header2'
 import Router2 from './router2'
 import Router3 from './router3'
 import FormText from './form-text'
+import FormSelectFile from './form-select-file'
 
 export default function App() {
-  return<FormText/>
+  return<FormSelectFile/>
 }
